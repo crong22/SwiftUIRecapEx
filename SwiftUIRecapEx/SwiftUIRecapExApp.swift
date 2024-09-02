d6e3893b9e5d22976820a70be85e36b65bd2c221
yeonwoo <mamen7624@gmail.com>
2024-09-03 01:30:42 +0900

    Initial Commit

---
+++ SwiftUIRecapEx/SwiftUIRecapExApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRecapExApp.swift
+//  SwiftUIRecapEx
+//
+//  Created by Woo on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRecapExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
