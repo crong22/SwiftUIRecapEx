c72e9cdccf9168b0aabe7dc5b52797b680105635
yeonwoo <mamen7624@gmail.com>
2024-09-03 01:57:10 +0900

    [1]

--- SwiftUIRecapEx/ContentView.swift
+++ SwiftUIRecapEx/ContentView.swift
@@ -9,13 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack{
+                VStack {
+                    Text(" ")
+                    Text("HELLO,")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("MY POLAAROID")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.blue)
+                .padding(.horizontal)
+                RoundedRectangle(cornerRadius: 25)
+                    .frame(height: 450)
+                    .padding(.horizontal)
+                Spacer()
+                NavigationLink("시작하기") {
+                    TransitionView()
+                }
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.white)
+                .background(.blue)
+            }
         }
-        .padding()
+//        .foregroundColor(.white)
+//        .frame(height: 35)
+//        .frame(maxWidth: .infinity)
+//        .background(.blue)
+//        .padding()
+    }
+}
+
+struct TransitionView : View {
+    
+    init() {
+        print("TransitionView init")
+    }
+    
+    var body: some View {
+        Text("🍟화면전환테스트🍟")
+            .onAppear{
+                print("TransitionView onAppear")
+            }
+            .onDisappear{
+                print("TransitionView onDisappear")
+            }
     }
 }
 
