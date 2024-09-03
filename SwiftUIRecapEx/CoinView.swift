878bc2888c27a27914a11f781752a1b84384472d
yeonwoo <mamen7624@gmail.com>
2024-09-04 01:38:06 +0900

    [CoinView] BannerView 생성 ( navigationTitle,  RoundedRectangle)

---
+++ SwiftUIRecapEx/CoinView.swift
@@ -0,0 +1,45 @@
+//
+//  CoinView.swift
+//  SwiftUIRecapEx
+//
+//  Created by Woo on 9/4/24.
+//
+
+import SwiftUI
+
+struct CoinView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                ZStack{
+                    bannerView()
+                }
+                .navigationTitle("My Money")
+                    }
+            }
+        }
+    }
+    
+    func bannerView() -> some View{
+        RoundedRectangle(cornerRadius: 25)
+            .fill(.blue)
+            .frame(height: 150)
+            .padding(.horizontal)
+            .overlay(alignment: .leading) {
+                Circle()
+                    .fill(.white.opacity(0.4))
+                    .scaleEffect(2)
+                VStack(alignment: .leading) {
+                    Spacer()
+                    Text("나의 쇼핑내역")
+                    Text("154,140,420")
+                }
+                .padding(25)
+                .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+
+#Preview {
+    CoinView()
+}
